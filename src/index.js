4ddb18b437e0872cc619d35b1c5129d0fa60ff40
Deepak Mali <deepak.mali@rubique.com>
2018-03-20 11:24:58 +0530

    modified height in getOption

--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ async function createFile(file1, file2, opts) {
 
 function getOptions(metadata1, metadata2, opts) {
   return {
-    width: metadata1.width + metadata2.width,
-    height: metadata1.height*2,
+    width: metadata1.width,
+    height: metadata1.height + metadata1.height,
     channels: metadata1.channels,
     format: opts.format ? opts.format : metadata1.format,
     output: opts.output || {},
